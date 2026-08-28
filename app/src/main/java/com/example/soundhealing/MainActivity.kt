cb7eb68bd5e7e780339c73c253c0132e726df36a
stand7hotmail-byte <stand7hotmail@gmail.com>
2026-08-28 19:43:01 +0900

    fix: Kotlin構文全面修正

--- app/src/main/java/com/example/soundhealing/MainActivity.kt
+++ app/src/main/java/com/example/soundhealing/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.soundhealing.ui.screen.MainScreen
 import com.example.soundhealing.ui.theme.SoundHealingTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/soundhealing/ui/theme/Theme.kt
+++ app/src/main/java/com/example/soundhealing/ui/theme/Theme.kt
@@ -36,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 fun SoundHealingTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
-    content: Composable() -> Unit
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
