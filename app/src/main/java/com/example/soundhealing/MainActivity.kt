503688b3c182d568ea9d6f6d156c202fab929c11
stand7hotmail-byte <stand7hotmail@gmail.com>
2026-08-28 19:28:20 +0900

    fix: Kotlin全面修正

--- app/src/main/java/com/example/soundhealing/MainActivity.kt
+++ app/src/main/java/com/example/soundhealing/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.soundhealing
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
